73d4c6f7676bcb1b29fad4fa1c0642d5489ea4db
aless133 <aless133@yandex.ru>
2022-12-04 19:54:11 +0300

    feat: add readline with basic events

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-console.log("I'm nodejs FM");
+import * as readline from "node:readline/promises";
+// import { stdin as input, stdout as output } from 'node:process';
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+rl.prompt();
+
+rl.on('line', (line) => {
+  switch (line.trim()) {
+  	case '.exit':
+  		rl.close();
+  		break;
+    case 'hello':
+      console.log('world!');
+      break;
+    default:
+      console.log(`Say what? I might have heard '${line.trim()}'`);
+      break;
+  }
+  rl.prompt();
+}).on('close', () => {
+  console.log('Thank you for using File Manager, Username, goodbye!');
+  process.exit(0);
+});
+
+rl.prompt();
+
+
+
+// rl.on("close", () => {
+// 	console.log("\nThank you for using File Manager, Username, goodbye!");
+// });
+
+// console.log("I'm nodejs FM");
+// while (1) {
+// 	const answer = await rl.question("What do you think of Node.js? ");
+// 	if (answer === ".exit") rl.close();
+// 	else console.log(`Thank you for your valuable feedback: ${answer}`);
+// }
+
+// rl.close();

d3750e3b2290c9a2359d9ea4c5df676b7070e6ea
Aless133 <aless133@yandex.ru>
2022-12-14 15:57:27 +0300

    feat: implement os/*

--- src/os.js
+++ src/os.js
@@ -7,6 +7,32 @@ const run = async (args) => {
 			checkArgvLength(args, 2);
 			console.log(Buffer.from(node_os.EOL).toString("hex"));
 			break;
+
+		case "--cpus":
+			checkArgvLength(args, 2);
+			console.table(
+				node_os.cpus().map((cpu) => ({
+					Cpu: cpu.model,
+					"Clock Rate": (cpu.speed / 1000).toFixed(1) + " GHz",
+				}))
+			);
+			break;
+
+		case "--homedir":
+			checkArgvLength(args, 2);
+			console.log(node_os.homedir());
+			break;
+
+		case "--username":
+			checkArgvLength(args, 2);
+			console.log(node_os.userInfo().username);
+			break;
+
+		case "--architecture":
+			checkArgvLength(args, 2);
+			console.log(process.platform, process.arch); //,node_os.machine(),node_os.arch()
+			break;
+
 		default:
 			throw Error("Invalid input");
 			break;
